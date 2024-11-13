33c18db9a5fbcdd969189ffe96ce0c34df81f60a
YuliaPanchenko <panchenkoyulia13@gmail.com>
2024-11-14 01:15:26 +0200

    Swagger documentation

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import cookieParser from 'cookie-parser';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const setupServer = () => {
   const app = express();
@@ -37,11 +38,12 @@ const setupServer = () => {
   });
 
   app.use(router);
+  app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
 
   app.use('*', notFoundHandler);
 
   app.use(errorHandler);
-  app.use('/uploads', express.static(UPLOAD_DIR));
 
   return app;
 };
